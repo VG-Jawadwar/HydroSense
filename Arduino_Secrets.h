d67f83dced447077f56eefb05740a34d581bad71
Vaibhav Jawadwar <154688684+VG-Jawadwar@users.noreply.github.com>
2025-06-15 20:57:00 +0530

    Update Arduino_Secrets.h

--- Arduino_Secrets.h
+++ Arduino_Secrets.h
@@ -5,4 +5,4 @@
 #define FIREBASE_HOST_URL "https://hydrosenseproj-default-rtdb.firebaseio.com"
 
 // FIREBASE DATABASE KEY
-#define FIREBASE_AUTH_KEY "AIzaSyA0GLKGHG7Q0dui5Pmdnug4ODB1XP1AHrA"
+#define FIREBASE_AUTH_KEY "Your Firebase Project API Key"
